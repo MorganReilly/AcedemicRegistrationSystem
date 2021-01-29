6f905a56717706de471a3550117fa6710337e880
Morgan Reilly <morgan.reilly27@gmail.com>
2021-01-29 19:19:54 +0000

    Parser Inheritence
    + Create individual parsers for specific requests
    - Remove Abort code for specific requests
    + Include abstracted abort code for general reuse

--- webapp/app.py
+++ webapp/app.py
@@ -1,25 +1,34 @@
+"""
+TITLE: Academic Registration System
+DESCRIPTION: Genesys Coding Challenge
+AUTHOR: Morgan Reilly
+"""
+# Imports
 from flask import Flask
 from flask_restful import reqparse, abort, Api, Resource
 
 app = Flask(__name__)
 api = Api(app)
 
+"""
+Mock Data for:
+Professors, Students, Courses, Course Registries
+
+Still need to hook up the database and use that instead
+"""
 PROFESSORS = {
     '1': {'name': 'Alice'},
     '2': {'name': 'Bob'}
 }
-
 STUDENTS = {
     '1': {'name': 'Greg'},
     '2': {'name': 'Tina'}
 }
-
 COURSES = {
     '101': {'name': 'Maths', 'p_id': '1'},
     '102': {'name': 'English', 'p_id': '2'},
     '103': {'name': 'Latin'}
 }
-
 COURSE_REGISTRIES = {
     '1': {'c_id': '100', 's_id': '1'},
     '2': {'c_id': '100', 's_id': '2'},
@@ -28,51 +37,46 @@ COURSE_REGISTRIES = {
 }
 
 
-def abort_if_prof_not_exist(p_id):
-    """Handles non existing professor"""
-    if p_id not in PROFESSORS:
-        abort(404, message="Professor {} does not exist".format(p_id))
+def abort_if_elem_not_exist(elem_id, elements, message):
+    """Handles non existing element"""
+    if elem_id not in elements:
+        abort(404, message="{} {} does not exist".format(message, elem_id))
 
 
-def abort_if_student_not_exist(s_id):
-    """Handles non existing student"""
-    if s_id not in STUDENTS:
-        abort(404, message="Student {} does not exist".format(s_id))
-
-
-def abort_if_course_not_exist(c_id):
-    """Handles non existing course"""
-    if c_id not in COURSES:
-        abort(404, message="Course {} does not exist".format(c_id))
-
-
-def abort_if_registry_not_exist(r_id):
-    """Handles non existing registry"""
-    if r_id not in COURSE_REGISTRIES:
-        abort(404, message="Course Registry {} does not exist".format(r_id))
-
-
-parser = reqparse.RequestParser()
-parser.add_argument('name')
-parser.add_argument('p_id')
-parser.add_argument('c_id')
-parser.add_argument('s_id')
+"""
+Parsers
+Used to handle various requests.
+I have split these up due to each request taking somewhat different values.
+"""
+# Parser for Professor Requests
+p_parser = reqparse.RequestParser(bundle_errors=True)  # Create a new parser
+p_parser.add_argument('name', type=str, required=True, help="Name cannot be blank")
+# Inherit parser arguments from professor requests,
+# but also include p_id since it's unique to course requests
+c_parser = p_parser.copy()
+c_parser.add_argument('p_id', type=int, required=False)
+# Student parser will only inherit name
+s_parser = p_parser.copy()
+# Course Registry is unique to requests in that it doesn't need a name, but instead has 2 id's passed
+r_parser = reqparse.RequestParser(bundle_errors=True)
+r_parser.add_argument('c_id', type=int, required=True, help="Course ID cannot be blank")
+r_parser.add_argument('s_id', type=int, required=True, help="Student ID cannot be blank")
 
 
 class Professor(Resource):
     """Handles single entries; GET, DELETE, PUT"""
 
     def get(self, p_id):
-        abort_if_prof_not_exist(p_id)
+        abort_if_elem_not_exist(p_id, PROFESSORS, "Professor")
         return PROFESSORS[p_id]
 
     def delete(self, p_id):
-        abort_if_prof_not_exist(p_id)
+        abort_if_elem_not_exist(p_id, PROFESSORS, "Professor")
         del PROFESSORS[p_id]
         return '', 204
 
     def put(self, p_id):
-        args = parser.parse_args()
+        args = p_parser.parse_args()
         professor = {'name': args['name']}
         PROFESSORS[p_id] = professor
         return professor, 201
@@ -85,7 +89,7 @@ class ProfessorList(Resource):
         return PROFESSORS
 
     def post(self):
-        args = parser.parse_args()
+        args = p_parser.parse_args()
         p_id = int(max(PROFESSORS.keys()).lstrip('professor')) + 1
         p_id = '%i' % p_id
         PROFESSORS[p_id] = {'name': args['name']}
@@ -96,16 +100,16 @@ class Student(Resource):
     """Handles single entries; GET, DELETE, PUT"""
 
     def get(self, s_id):
-        abort_if_student_not_exist(s_id)
+        abort_if_elem_not_exist(s_id, STUDENTS, "Student")
         return STUDENTS[s_id]
 
     def delete(self, s_id):
-        abort_if_student_not_exist(s_id)
+        abort_if_elem_not_exist(s_id, STUDENTS, "Student")
         del STUDENTS[s_id]
         return '', 204
 
     def put(self, s_id):
-        args = parser.parse_args()
+        args = s_parser.parse_args()
         student = {'name': args['name']}
         STUDENTS[s_id] = student
         return student, 201
@@ -118,7 +122,7 @@ class StudentList(Resource):
         return STUDENTS
 
     def post(self):
-        args = parser.parse_args()
+        args = s_parser.parse_args()
         s_id = int(max(STUDENTS.keys()).lstrip('student')) + 1
         s_id = '%i' % s_id
         STUDENTS[s_id] = {'name': args['name']}
@@ -129,16 +133,16 @@ class Course(Resource):
     """Handles single entries; GET, DELETE, PUT"""
 
     def get(self, c_id):
-        abort_if_course_not_exist(c_id)
+        abort_if_elem_not_exist(c_id, COURSES, "Course")
         return COURSES[c_id]
 
     def delete(self, c_id):
-        abort_if_course_not_exist(c_id)
+        abort_if_elem_not_exist(c_id, COURSES, "Course")
         del COURSES[c_id]
         return '', 204
 
     def put(self, c_id):
-        args = parser.parse_args()
+        args = c_parser.parse_args()
         course = {'name': args['name'], 'p_id': args['p_id']}
         COURSES[c_id] = course
         return course, 201
@@ -151,7 +155,7 @@ class CourseList(Resource):
         return COURSES
 
     def post(self):
-        args = parser.parse_args()
+        args = c_parser.parse_args()
         c_id = int(max(COURSES.keys()).lstrip('course')) + 1
         c_id = '%i' % c_id
         COURSES[c_id] = {'name': args['name'], 'p_id': args['p_id']}
@@ -162,16 +166,16 @@ class CourseRegistry(Resource):
     """Handles single entries; GET, DELETE, PUT"""
 
     def get(self, r_id):
-        abort_if_registry_not_exist(r_id)
+        abort_if_elem_not_exist(r_id, COURSE_REGISTRIES, "Course Registry")
         return COURSE_REGISTRIES[r_id]
 
     def delete(self, r_id):
-        abort_if_registry_not_exist(r_id)
+        abort_if_elem_not_exist(r_id, COURSE_REGISTRIES, "Course Registry")
         del COURSE_REGISTRIES[r_id]
         return '', 204
 
     def put(self, r_id):
-        args = parser.parse_args()
+        args = r_parser.parse_args()
         course_registry = {'c_id': args['c_id'], 's_id': args['s_id']}
         COURSE_REGISTRIES[r_id] = course_registry
         return course_registry, 201
@@ -184,7 +188,7 @@ class CourseRegistryList(Resource):
         return COURSE_REGISTRIES
 
     def post(self):
-        args = parser.parse_args()
+        args = r_parser.parse_args()
         r_id = int(max(COURSE_REGISTRIES.keys()).lstrip('course_registry')) + 1
         r_id = '%i' % r_id
         COURSE_REGISTRIES[r_id] = {'c_id': args['c_id'], 's_id': args['s_id']}
