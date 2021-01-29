111a8cc3b3ca0ef8dd4069cff4c1496772835d7a
Morgan Reilly <morgan.reilly27@gmail.com>
2021-01-29 15:39:42 +0000

    Full CRUD Course Route
    Need to look at design choices here... Not sure if I want to have to include the professor id with each payload
    + GET, PUT, POST, DELETE funtionality included

--- webapp/app.py
+++ webapp/app.py
@@ -14,6 +14,12 @@ STUDENTS = {
     '2': {'name': 'Tina'}
 }
 
+COURSES = {
+    '101': {'name': 'Maths', 'p_id': '1'},
+    '102': {'name': 'English', 'p_id': '2'},
+    '103': {'name': 'Latin'}
+}
+
 
 def abort_if_prof_not_exist(p_id):
     """Handles non existing professor"""
@@ -27,8 +33,15 @@ def abort_if_student_not_exist(s_id):
         abort(404, message="Student {} does not exist".format(s_id))
 
 
+def abort_if_course_not_exist(c_id):
+    """Handles non existing student"""
+    if c_id not in COURSES:
+        abort(404, message="Course {} does not exist".format(c_id))
+
+
 parser = reqparse.RequestParser()
 parser.add_argument('name')
+parser.add_argument('p_id')
 
 
 class Professor(Resource):
@@ -97,11 +110,46 @@ class StudentList(Resource):
         return STUDENTS[s_id], 201
 
 
+class Course(Resource):
+    """Handles single entries; GET, DELETE, PUT"""
+
+    def get(self, c_id):
+        abort_if_course_not_exist(c_id)
+        return COURSES[c_id]
+
+    def delete(self, c_id):
+        abort_if_course_not_exist(c_id)
+        del COURSES[c_id]
+        return '', 204
+
+    def put(self, c_id):
+        args = parser.parse_args()
+        course = {'name': args['name'], 'p_id': args['p_id']}
+        COURSES[c_id] = course
+        return course, 201
+
+
+class CourseList(Resource):
+    """Handles multiple entries: GET, POST"""
+
+    def get(self):
+        return COURSES
+
+    def post(self):
+        args = parser.parse_args()
+        c_id = int(max(COURSES.keys()).lstrip('course')) + 1
+        c_id = '%i' % c_id
+        COURSES[c_id] = {'name': args['name'], 'p_id': args['p_id']}
+        return COURSES[c_id], 201
+
+
 # API Resource Routing
 api.add_resource(ProfessorList, '/professors')
 api.add_resource(Professor, '/professors/<p_id>')
 api.add_resource(StudentList, '/students')
 api.add_resource(Student, '/students/<s_id>')
+api.add_resource(CourseList, '/courses')
+api.add_resource(Course, '/courses/<c_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
