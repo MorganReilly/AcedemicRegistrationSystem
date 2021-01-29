3e3322d6e7fa762e6452a2804b77f6b50fd52ede
Morgan Reilly <morgan.reilly27@gmail.com>
2021-01-29 14:31:00 +0000

    Full CRUD for Student Route
    + GET, PUT, POST, DELETE functionality included

--- webapp/app.py
+++ webapp/app.py
@@ -9,11 +9,22 @@ PROFESSORS = {
     '2': {'name': 'Bob'}
 }
 
+STUDENTS = {
+    '1': {'name': 'Greg'},
+    '2': {'name': 'Tina'}
+}
 
-def abort_if_prof_not_exist(prof_id):
+
+def abort_if_prof_not_exist(p_id):
     """Handles non existing professor"""
-    if prof_id not in PROFESSORS:
-        abort(404, message="Professor {} does not exist".format(prof_id))
+    if p_id not in PROFESSORS:
+        abort(404, message="Professor {} does not exist".format(p_id))
+
+
+def abort_if_student_not_exist(s_id):
+    """Handles non existing student"""
+    if s_id not in STUDENTS:
+        abort(404, message="Student {} does not exist".format(s_id))
 
 
 parser = reqparse.RequestParser()
@@ -23,19 +34,19 @@ parser.add_argument('name')
 class Professor(Resource):
     """Handles single entries; GET, DELETE, PUT"""
 
-    def get(self, prof_id):
-        abort_if_prof_not_exist(prof_id)
-        return PROFESSORS[prof_id]
+    def get(self, p_id):
+        abort_if_prof_not_exist(p_id)
+        return PROFESSORS[p_id]
 
-    def delete(self, prof_id):
-        abort_if_prof_not_exist(prof_id)
-        del PROFESSORS[prof_id]
+    def delete(self, p_id):
+        abort_if_prof_not_exist(p_id)
+        del PROFESSORS[p_id]
         return '', 204
 
-    def put(self, prof_id):
+    def put(self, p_id):
         args = parser.parse_args()
         professor = {'name': args['name']}
-        PROFESSORS[prof_id] = professor
+        PROFESSORS[p_id] = professor
         return professor, 201
 
 
@@ -47,15 +58,50 @@ class ProfessorList(Resource):
 
     def post(self):
         args = parser.parse_args()
-        prof_id = int(max(PROFESSORS.keys()).lstrip('professor')) + 1
-        prof_id = '%i' % prof_id
-        PROFESSORS[prof_id] = {'name': args['name']}
-        return PROFESSORS[prof_id], 201
+        p_id = int(max(PROFESSORS.keys()).lstrip('professor')) + 1
+        p_id = '%i' % p_id
+        PROFESSORS[p_id] = {'name': args['name']}
+        return PROFESSORS[p_id], 201
+
+
+class Student(Resource):
+    """Handles single entries; GET, DELETE, PUT"""
+
+    def get(self, s_id):
+        abort_if_student_not_exist(s_id)
+        return STUDENTS[s_id]
+
+    def delete(self, s_id):
+        abort_if_student_not_exist(s_id)
+        del STUDENTS[s_id]
+        return '', 204
+
+    def put(self, s_id):
+        args = parser.parse_args()
+        student = {'name': args['name']}
+        STUDENTS[s_id] = student
+        return student, 201
+
+
+class StudentList(Resource):
+    """Handles multiple entries: GET, POST"""
+
+    def get(self):
+        return STUDENTS
+
+    def post(self):
+        args = parser.parse_args()
+        s_id = int(max(STUDENTS.keys()).lstrip('student')) + 1
+        s_id = '%i' % s_id
+        STUDENTS[s_id] = {'name': args['name']}
+        return STUDENTS[s_id], 201
 
 
 # API Resource Routing
 api.add_resource(ProfessorList, '/professors')
-api.add_resource(Professor, '/professors/<prof_id>')
+api.add_resource(Professor, '/professors/<p_id>')
+api.add_resource(StudentList, '/students')
+api.add_resource(Student, '/students/<s_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
