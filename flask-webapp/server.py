4d6bd0f6d797391832a30f3ac3713cdd2854d0bc
Morgan Reilly <morgan.reilly27@gmail.com>
2021-02-01 14:13:18 +0000

    Update swagger.yml
    Tidied up the file by adding unambiguious descriptions of each endpoint

--- flask-webapp/server.py
+++ flask-webapp/server.py
@@ -16,12 +16,5 @@ connex_app = config.connex_app
 # Read swagger.yml file to configure endpoints
 connex_app.add_api('swagger.yml')
 
-
-# Create URL route in application for "/"
-@connex_app.route('/')
-def home():
-    return render_template('home.html')
-
-
 if __name__ == '__main__':
     connex_app.run(host='0.0.0.0', port=5000, debug=True)
