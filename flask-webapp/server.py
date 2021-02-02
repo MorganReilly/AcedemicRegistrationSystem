53b63996be0f12802713d19dddd31a9e1b6f3689
Morgan Reilly <morgan.reilly27@gmail.com>
2021-02-02 10:53:37 +0000

    Reincluded '/' route and landing page
    Just want to verify that when I host the app it's working correctly, bit silly to remove it...

--- flask-webapp/server.py
+++ flask-webapp/server.py
@@ -8,7 +8,7 @@ LINKS:
     * https://realpython.com/flask-connexion-rest-api/
     * https://dev.mysql.com/doc/connector-python/en/connector-python-introduction.html
 """
-from flask import Flask
+from flask import Flask, render_template
 import config  # Load the configuration from config.py
 
 # Get app instance
@@ -16,6 +16,13 @@ connex_app = config.connex_app
 
 # Read swagger.yml file to configure endpoints
 connex_app.add_api('swagger.yml')
+
+
+# Create URL route in application for "/"
+@connex_app.route('/')
+def home():
+    return render_template('home.html')
+
 
 if __name__ == '__main__':
     connex_app.run(host='0.0.0.0', port=5000, debug=True)
