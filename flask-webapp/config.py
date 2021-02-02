19ec94f807eb28683fd461f012a0edcff903ecec
Morgan Reilly <morgan.reilly27@gmail.com>
2021-02-02 16:18:48 +0000

    Updated RDS endpoint

--- flask-webapp/config.py
+++ flask-webapp/config.py
@@ -17,16 +17,16 @@ connex_app = connexion.App(__name__, specification_dir=basedir)
 app = connex_app.app
 
 """MySQL Local Configuration"""
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'morgan'
-app.config['MYSQL_PASSWORD'] = 'rootpassword'
-app.config['MYSQL_DB'] = 'academicdb'
-
-"""MySQL AWS Configuration"""
-# app.config['MYSQL_HOST'] = 'academic-database.cykgq28ntsef.eu-west-1.rds.amazonaws.com'
-# app.config['MYSQL_USER'] = 'admin'
+# app.config['MYSQL_HOST'] = 'localhost'
+# app.config['MYSQL_USER'] = 'morgan'
 # app.config['MYSQL_PASSWORD'] = 'rootpassword'
 # app.config['MYSQL_DB'] = 'academicdb'
+
+"""MySQL AWS Configuration"""
+app.config['MYSQL_HOST'] = 'academic-database.cykgq28ntsef.eu-west-1.rds.amazonaws.com'
+app.config['MYSQL_USER'] = 'admin'
+app.config['MYSQL_PASSWORD'] = 'rootpassword'
+app.config['MYSQL_DB'] = 'academicdb'
 
 # Create MySQL db instance
 db = MySQL(app)
