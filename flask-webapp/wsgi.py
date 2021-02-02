4e8deba9d6a8e6888dc394058d6e615e437eff8a
Morgan Reilly <morgan@ip-172-31-33-35.eu-west-1.compute.internal>
2021-02-02 21:55:15 +0000

    Add Server Configuration

---
+++ flask-webapp/wsgi.py
@@ -0,0 +1,12 @@
+"""
+TITLE: courses.py
+ABOUT: WSGI Entry Point
+AUTHOR: Morgan Reilly
+LINKS:
+    * https://www.digitalocean.com/community/tutorials/how-to-serve-flask-applications-with-uwsgi-and-nginx-on-ubuntu-20-04
+"""
+from server import connex_app as app
+
+
+if __name__ == "__main__":
+    app.run()
